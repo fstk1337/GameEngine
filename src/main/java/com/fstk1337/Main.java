5a22a704ef861faf8fb9cd895d51b1f374110308
fstk1337 <vit.shvaichuk@gmail.com>
2022-01-10 20:44:04 +0300

    red window created.

---
+++ src/main/java/com/fstk1337/Main.java
@@ -0,0 +1,10 @@
+package com.fstk1337;
+
+import com.fstk1337.jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
